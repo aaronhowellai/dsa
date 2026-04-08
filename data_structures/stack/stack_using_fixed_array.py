1cb487203817da9358e5dc96e842450141321d4a
AaronHowellGDSGroup <aaron.howell@gdsgroup.com>
2026-04-08 13:09:00 +0100

    scaffold the fixed array class.

---
+++ data_structures/stack/stack_using_fixed_array.py
@@ -0,0 +1,30 @@
+"""
+
+Implementation of a stack using a fixed array with push/pop operations on integers and strings in O(1) time, to simulate the function of undo/redo.
+
+This will use parallel stacks:
+- data_stack: where the current working data is stored.
+- history_stack: where the history that you can undo from stack(a) to stack(b) is stored as well as redo from stack(b) to stack(a).\
+
+"""
+
+class fixedArraryStack():
+
+    def __init__(self,stack1,stack2,size):
+        pass
+
+    def stackPop():
+        pass
+
+    def stackPush():
+        pass
+
+    def undo():
+        pass
+
+    def redo():
+        pass
+
+    def returnStack():
+        pass
+
